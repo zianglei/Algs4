2b2e73e3da44f33be09adf366c47c450215f4fa5
zianglei <zianglei@126.com>
2020-09-16 11:32:54 +0800

    full marks

--- burrows/CircularSuffixArray.java
+++ burrows/CircularSuffixArray.java
@@ -1,11 +1,10 @@
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Arrays;
-
 public class CircularSuffixArray {
 
     private int length;
     private Integer[] indices;
+    private static final int CUTOFF = 15;
 
 
     // circular suffix array of s
@@ -18,17 +17,58 @@ public class CircularSuffixArray {
         for (int i = 0; i < length; i++) {
             indices[i] = i;
         }
-        Arrays.sort(indices, (p1, p2) -> {
-            for (int i = 0; i < length; i++) {
-                int a1 = (p1 + i) % length;
-                int a2 = (p2 + i) % length;
-                if (s.charAt(a1) == s.charAt(a2))
-                    continue;
-                return Character.compare(s.charAt(a1), s.charAt(a2));
-            }
-            return 0;
-        });
+        sort(s, 0, length - 1, 0);
     }
+
+    private void sort(String s, int lo, int hi, int d) {
+        if (hi <= lo + CUTOFF) {
+            insertion(s, lo, hi, d);
+            return;
+        }
+        int lt = lo, gt = hi;
+        int v = charAt(s, indices[lo], d);
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = charAt(s, indices[i], d);
+            if (t < v) exch(lt++, i++);
+            else if (t > v) exch(i, gt--);
+            else i++;
+        }
+
+        sort(s, lo, lt - 1, d);
+        if (v >= 0) sort(s, lt, gt, d + 1);
+        sort(s, gt + 1, hi, d);
+    }
+
+    private void insertion(String s, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(s, indices[j], indices[j - 1], d); j--) {
+                exch(j, j - 1);
+            }
+        }
+    }
+
+    private int charAt(String s, int start, int offset) {
+        if (offset == length) return -1;
+        return s.charAt((start + offset) % length);
+    }
+
+    private boolean less(String s, int v, int w, int d) {
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.charAt((v + i) % len) < s.charAt((w + i) % len)) return true;
+            if (s.charAt((v + i) % len) > s.charAt((w + i) % len)) return false;
+        }
+        return false;
+    }
+
+    private void exch(int i, int j) {
+        int temp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = temp;
+    }
+
+
 
     // length of s
     public int length() {
